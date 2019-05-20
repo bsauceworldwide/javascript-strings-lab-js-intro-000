cf7204e5888ebfc6a086b9d9fc64b906a1664b8e
brad.schmalz <brad.schmalz@gmail.com>
2019-05-20 21:35:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-var greeting = "Hello, everybody!"
-
+var greeting = "Hello, everybody!";
+ 
 var specialGuest = "Neil deGrasse Tyson"
-"Hello, " + specialGuest + "!"
 
+ 
+var greetSpecialGuest = "" + specialGuest + "!";
+ 
 var topic = "space";
-
+ 
 var conversation = `${topic}`;
-`Let\'s talk about ${topic}`
